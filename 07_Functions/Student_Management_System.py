59498922f33a60d1adf5fd90a4ad1fd26b97e4d5
Amaan Shaikh <amaan.shaikh2006@outlook.com>
2025-11-27 17:15:21 +0530

    Add Function Project

---
+++ 07_Functions/Student_Management_System.py
@@ -0,0 +1,69 @@
+def menu():
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Search Student")
+    print("4. Remove Student")
+    print("5. Exit")    
+
+def add_student(students):
+    name = input("Enter student name: ")
+    roll = input("Enter roll number of student: ")
+    grade = input("Enter class of student: ")
+
+    student = {
+        "name": name,
+        "roll": roll,
+        "grade": grade
+    }
+    students.append(student)
+    print("Student added!\n")
+
+def view_students(students):
+    if not students:
+        print("No students found.\n")
+        return
+
+    print("\n--- Student List ---")
+    for s in students:
+        print(f"{s['roll']} - {s['name']} ({s['grade']})")
+    print()
+
+def search_student(students):
+    roll = input("Enter roll to search: ")
+
+    for s in students:
+        if s['roll'] == roll:
+            print("Student Found:", s)
+            print()
+            return
+    print("Student not found.\n")
+
+def remove_student(students):
+    roll = input("Enter roll to remove: ")
+
+    for s in students:
+        if s['roll'] == roll:
+            students.remove(s)
+            print("Student removed!\n")
+            return
+    print("Student not found.\n")
+    
+students = []
+
+
+while True:
+    menu()
+    ch = input("Choose option: ")
+
+    if ch == "1":
+        add_student(students)
+    elif ch == "2":
+        view_students(students)
+    elif ch == "3":
+        search_student(students)
+    elif ch == "4":
+        remove_student(students)
+    elif ch == "5":
+        break
+    else:
+        print("Invalid option\n")
